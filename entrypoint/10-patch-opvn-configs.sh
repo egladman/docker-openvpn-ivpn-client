40d5ec47be89ea5812a6f6e42f950f87c46d6a42
Eli Gladman <8713153+egladman@users.noreply.github.com>
2021-12-04 14:13:02 -0500

    remove sed dependency

--- entrypoint/10-patch-opvn-configs.sh
+++ entrypoint/10-patch-opvn-configs.sh
@@ -10,6 +10,12 @@ _log() {
     printf '%b\n' "[${1:: 4}] ${now} ${0##*/} ${2}"
 }
 
+log::debug() {
+    [[ $DEBUG -ne 1 ]] && return
+
+    _log "DEBUG" "$*"
+}
+
 log::warn() {
     _log "WARN" "$*"
 }
@@ -22,7 +28,32 @@ main() {
     local config_dir="${CONFIG_DIR:-/config/client}"
     local pass_file="${PASS_FILE:-/config/credentials}"
 
-    sed -i -r "s/auth-user-pass[[:blank:]]?$/auth-user-pass ${pass_file}/" "${config_dir}"/*.ovpn
+    # This could be accomplished with a sed one-liner, but where's the fun in
+    # that. Pure bash solution ftw
+
+    regex="^auth-user-pass([[:blank:]]){0,}"
+    for conf in "${config_dir}"/*.ovpn; do
+        conf_patched=0
+
+        while read -r line; do
+            conf_data+=("$line")
+        done < "$conf"
+
+        for i in "${!conf_data[@]}"; do
+            if [[ "${conf_data[$i]}" =~ $regex ]] && [[ "${conf_data[$i]}" != *"${pass_file}" ]]; then
+                conf_data[$i]="auth-user-pass ${pass_file}"
+                conf_patched=1
+            fi
+        done
+
+        if [[ $conf_patched -eq 0 ]]; then
+            log::debug "Skipping config '$conf'."
+            continue
+        fi
+
+        log::debug "Patching config '$conf'."
+        printf '%s\n' "${conf_data[@]}" > "$conf"
+    done
 
     if [[ -z "$USERNAME" ]]; then
         log::warn "Environment variable 'USERNAME' is unset."
@@ -33,9 +64,13 @@ main() {
     fi
 
     if [[ -f "$pass_file" ]]; then # A pass file was bind mounted in
+        log::debug "Found openvpn credentials file."
         return
     fi
 
+    log::debug "Creating credentials file '$pass_file'."
+
+    # shellcheck disable=SC2188
     >"$pass_file"
     chmod 600 "$pass_file"
 
