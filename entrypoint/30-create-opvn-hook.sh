b1e599f45bbdcdfff3d21ed3be235bf705740ee6
Eli Gladman <8713153+egladman@users.noreply.github.com>
2021-12-18 12:47:26 -0500

    refactor

--- entrypoint/30-create-opvn-hook.sh
+++ entrypoint/30-create-opvn-hook.sh
@@ -5,5 +5,10 @@ set -o pipefail -o errexit
 # Used in combanation with openvpn option '--up'. Create a file named
 # openvpn_isready once the openvpn process is operational
 
-printf '%s\n%s\n' "#!/bin/bash" ">/tmp/openvpn_isready" > /tmp/openvpn_up
-chmod +x /tmp/openvpn_up
+main() {
+    log::debug "Creating file '/tmp/openvpn_up'."
+    printf '%s\n%s\n' "#!/bin/bash" ">/tmp/openvpn_isready" > /tmp/openvpn_up
+    chmod +x /tmp/openvpn_up
+}
+
+main
