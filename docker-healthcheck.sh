bf881c67d32714242a667dde3523fcd745cc2890
Eli Gladman <8713153+egladman@users.noreply.github.com>
2021-12-18 12:51:56 -0500

    make shellcheck happy

--- docker-healthcheck.sh
+++ docker-healthcheck.sh
@@ -13,8 +13,9 @@ main() {
     printf '%s\n' "Pinging '${tuple[1]}'."
 
     ping -c 1 -w 5 "${tuple[1]}"
+    # shellcheck disable=SC2181
     if [[ $? -ne 0 ]]; then
-        printf "Command 'ping' returned non-zero code. Failed to establish connection to '${tuple[1]}'."
+        printf '%s\n' "Command 'ping' returned non-zero code. Failed to establish connection to '${tuple[1]}'."
         exit 1
     fi
 }
